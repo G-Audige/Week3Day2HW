42c4dc6ef6d0cbb964c73ff6347775161f3ec7fa
G-Audige <gloriaaudige@gmail.com>
2023-10-21 13:29:48 -0400

    Pushing to GitHub

---
+++ Week3Day2HW.js
@@ -0,0 +1,20 @@
+class Cat{
+    constructor(breed, eyes, fur, age) {
+        this.breed = breed;
+        this.eyes = eyes;
+        this.fur = fur;
+        this.age = age
+    }
+
+}
+
+const lux = new Cat("Siamese", "Blue", "Lilac Point", 4)
+const mrbarbatos = new Cat("Persian", "Green", "Gray", 13)
+console.log(lux)
+console.log(mrbarbatos)
+
+class Pirate {
+    constructor(){
+        
+    }
+}
